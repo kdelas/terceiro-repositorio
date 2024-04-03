0ad647a6d4823419c566e3d95d335a92cb4ae15e
kdelas <108105845+kdelas@users.noreply.github.com>
2024-04-02 22:46:51 -0300

    05/02

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ for (let i=0; i<botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "contagem regressiva";
+const tempoObjetivol = new Date("2024-05-10T23:59:59");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
