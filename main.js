d65bec223e6b19f04e269d27b5cf33d44f3f201c
kdelas <108105845+kdelas@users.noreply.github.com>
2024-04-09 22:50:43 -0300

    09/04

--- main.js
+++ main.js
@@ -16,9 +16,24 @@ for (let i=0; i<botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-
-contadores[0].textContent = "contagem regressiva";
 const tempoObjetivol = new Date("2024-05-10T23:59:59");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual= new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "munitos" + segundos + "segundos";
+}
+
+tempoObjetivo1 - tempoAtual
