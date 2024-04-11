d62aa8bce2c93fae829f46eb3fc96001331d35bd
kdelas <108105845+kdelas@users.noreply.github.com>
2024-04-11 20:10:38 -0300

    11/04

--- main.js
+++ main.js
@@ -27,7 +27,8 @@ function calculaTempo(tempoObjetivo){
 
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(horas/24);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor (hoas/24);
 
     segundos %=60;
     minutos %=60;
